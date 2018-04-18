bb3f14fc003056c89369dd17462ecb573928d3aa
IEUser <IEUser@192.168.28.190>
2018-04-18 11:52:40 -0700

    holigit

---
+++ HolaGit/src/GitHoli.java
@@ -0,0 +1,21 @@
+
+public class GitHoli {
+	
+	  public static void main(String[] args)
+	    {
+	        System.out.println("Hola usuario bienvenido");
+	        System.out.println("A continuación se muestra una cuenta regresiva");
+
+	        for (int i = 10; i >= 1;  i--)
+	        {
+	            System.out.println(i);
+	        }
+
+	        System.out.println("Ahora, otra cuenta regresiva");
+
+	        for (int i = 10; i >= 1;  i--)
+	        {
+	            System.out.print(i);
+	        }
+	    }
+}
